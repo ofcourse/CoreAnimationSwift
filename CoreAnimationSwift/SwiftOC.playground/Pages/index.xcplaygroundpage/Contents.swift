1d851596ec7d6efb2820ce0ef48e93e9a691a3e7
hsg <hushengguo@dobechina.com>
2017-10-27 19:34:11 +0800

    swift interact with oc

---
+++ CoreAnimationSwift/SwiftOC.playground/Pages/index.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+/*:
+ # 目录
+ 1. [swift working with cocoa frameworks](working_with_cocoa)
+ 1. [Interacting with Objective-C APIs](interactingWithocAPI)
+ 1. [Interacting with C APIs](interactingWithCAPI)
+ 
+ [more...](https://developer.apple.com/library/content/documentation/Swift/Conceptual/BuildingCocoaApps/index.html#//apple_ref/doc/uid/TP40014216-CH2-ID0)
+ *
+ *
+ */
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
---
+++ CoreAnimationSwift/SwiftOC.playground/Pages/working_with_cocoa.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+/*:
+ # working with cocoa
+ */
+import UIKit
+
+let number = 42
+let bridgedNumber: NSNumber = number as NSNumber
+
+let integerLiteral: NSNumber = 5
+let floatLiteral: NSNumber = 3.14159
+let booleanLiteral: NSNumber = true
+booleanLiteral.uint16Value
+
