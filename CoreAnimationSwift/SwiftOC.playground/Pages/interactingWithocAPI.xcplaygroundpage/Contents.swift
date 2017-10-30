039e8b36c100fc962e0b39671bee64d928de1b7a
hsg <hushengguo@dobechina.com>
2017-10-30 13:52:07 +0800

    swift&oc

--- CoreAnimationSwift/SwiftOC.playground/Pages/interactingWithocAPI.xcplaygroundpage/Contents.swift
+++ CoreAnimationSwift/SwiftOC.playground/Pages/interactingWithocAPI.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
 //: [Previous](@previous)
+/*:
+ # swift与oc api相互调用
+ */
+/*:
+ ## 初始化
+ * oc中初始化方法init与initWith，在swift中转化后会直接被去掉
+ * swift 中不需要调用alloc,swift会自动处理
+ */
+//in oc    :  UITableView *myTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+//in swift :  let myTableView: UITableView = UITableView(frame: .zero, style: .grouped)
 
 import Foundation
-
-var str = "Hello, playground"
-
+import UIKit
+/*:
+ ## 工厂方法及便利初始化
+ * 为了一致性与简洁性，在swift中oc中的工厂方法被导入成便利初始化方法。
+ */
+//in oc : UIColor *color = [UIColor colorWithRed:0.5 green:0.0 blue:0.5 alpha:1.0];
+//in swift: let color = UIColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 1.0)
+/*:
+ ## 失败的初始化
+  * oc init(),init!()代表初始化不会失败， oc init?()代表初始化可能失败
+ */
+if let image = UIImage(contentsOfFile: "MyImage.png") {
+} else {
+}
+/*:
+ ## 访问属性
+ *
+ */
+/*:
+ ## 对象比较swift
+ * == 比较对象内容
+ * === 比较对象指针是否指向同一块内存区
+ */
 //: [Next](@next)
--- CoreAnimationSwift/SwiftOC.playground/Pages/working_with_cocoa.xcplaygroundpage/Contents.swift
+++ CoreAnimationSwift/SwiftOC.playground/Pages/working_with_cocoa.xcplaygroundpage/Contents.swift
@@ -1,9 +1,26 @@
 //: Playground - noun: a place where people can play
 /*:
  # working with cocoa
+ * swift中可以使用oc API, oc 也可以调用swift api
+ * oc与c支持模块的框架都能直接导入到swift中引用. 包括所有的oc系统框架及通用的C类库等，如 Foundation, UIKit, and SpriteKit等
+ * import Foundation后，swift文件能访问所有Foundation的类，协议，方法，属性，常量等
  */
 import UIKit
 
+/*:
+ ## swift  import 框架的处理过程
+ oc代码先在头文件暴露接口，在swift中，这些头文件先被编译成oc模块，然后被导入成swift api. 导入过程决定oc中的函数，类，方法，类型在swift中的显示。
+ ### 类型转换
+ *  类型映射-映射oc类型到相等的swift类型，如 id到Any
+ *  核心类型映射-映射oc核心类型到可选择的swift类型 如NSString到String
+ *  概念映射-映射oc概念到swift,如指错到optionals类型
+ *  swift不支持映射C++代码，可以将c++代码用oc或c包装
+ */
+
+/*:
+ ## oc import swift处理过程与swift import 框架类似
+ * swift模块会生成oc头文件，头文件的api, 头文件的api可以映射回oc,有些swift api不能映射回oc，因为有些swift特性，oc无对应的特性来支持。
+ */
 let number = 42
 let bridgedNumber: NSNumber = number as NSNumber
 
@@ -11,4 +28,11 @@ let integerLiteral: NSNumber = 5
 let floatLiteral: NSNumber = 3.14159
 let booleanLiteral: NSNumber = true
 booleanLiteral.uint16Value
+
+import Foundation
+let a : NSNumber = 10
+/*:
+ ## cocoa framework
+ 桥接类型 swift自动将一些oc类型转换为swift类型，将一些swift类型转换为oc类型
+ */
 
