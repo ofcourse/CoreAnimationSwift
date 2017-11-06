2fd4f86679ca6c670f7d158a6b75b7b574304916
hsg <hushengguo@dobechina.com>
2017-11-06 18:26:44 +0800

    类方法class&static区别， static的类方法不能override但class修饰的类方法可以override

--- CoreAnimationSwift/SwiftOC.playground/Pages/playSwift.xcplaygroundpage/Contents.swift
+++ CoreAnimationSwift/SwiftOC.playground/Pages/playSwift.xcplaygroundpage/Contents.swift
@@ -15,6 +15,25 @@ class SomeClass {
     func doItOnlyIfInstanceOfThisType() {
         print("I'm a instance method. I belong to my type instance.")
     }
+    
+    class func work() {
+        print("Type Method: Person: 警察.")
+    }
+    
+    // 使用static关键字创建类方法
+    static func nameOfPerson(name: String) {
+        print("Type Method: Person name: \(name)")
+    }
+}
+
+class InSome:SomeClass {
+    override class func work() {
+        
+    }
+    //cannot override static method
+    /*override static func nameOfPerson(name: String) {
+        
+    }*/
 }
 
 //https://stackoverflow.com/questions/35035185/swift-metatype-type-self
@@ -23,7 +42,7 @@ cls.doIt()
 let cls2 : SomeClass = SomeClass() //实例
 cls2.doItOnlyIfInstanceOfThisType()
 
-func t() {
+/*func t() {
     Observable<Int>.from([1, 2, 3, -1, 5])
         .map { value -> Int in
             if value > 0 {
@@ -35,7 +54,10 @@ func t() {
         }
         .debug()
         .subscribe()
-        .disposed(by: disposeBag)
+        //.disposed(by: disposeBag)
 }
-t()
+t()*/
+SomeClass.work()
+SomeClass.nameOfPerson(name: "static name")
 
+//static & class 类方法的区别: 都是类方法，但是static的方法不能重写，但class的可以 http://www.jianshu.com/p/a9c9e7313438
